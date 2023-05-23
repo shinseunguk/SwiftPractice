69e40238f0003a82adc2ae9c0f7e94ce18e45241
신승욱 <ukseung.dev@gmail.com>
2023-05-23 22:34:53 +0900

    핫스팟 연결, 불필요한 속성 제거

--- SwiftPractice/View/Signing & Capabilities/HotspotConfigurationViewController.swift
+++ SwiftPractice/View/Signing & Capabilities/HotspotConfigurationViewController.swift
@@ -16,16 +16,6 @@ final class HotspotConfigurationViewController: UIViewController, UIViewControll
     
     var navTitle: String?
     
-    let ssidTextField = UITextField().then {
-        $0.placeholder = "ssidTextField"
-        $0.borderStyle = .roundedRect
-    }
-    
-    let passwordTextField = UITextField().then {
-        $0.placeholder = "passwordTextField"
-        $0.borderStyle = .roundedRect
-    }
-    
     lazy var button = UIButton(type: .system).then {
         $0.setTitle("connectToHotspot", for: .normal)
     }
@@ -51,30 +41,13 @@ final class HotspotConfigurationViewController: UIViewController, UIViewControll
     
     func setUI() {
         self.view.backgroundColor = .white
-        
-        self.view.addSubview(ssidTextField)
-        self.view.addSubview(passwordTextField)
         self.view.addSubview(button)
         self.view.addSubview(wifiInfoLabel)
     }
     
     func setAttributes() {
-        ssidTextField.snp.makeConstraints {
-            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(30)
-            $0.width.equalTo(300)
-            $0.height.equalTo(50)
-            $0.centerX.equalToSuperview()
-        }
-        
-        passwordTextField.snp.makeConstraints {
-            $0.top.equalTo(ssidTextField.snp.bottom).offset(10)
-            $0.width.equalTo(300)
-            $0.height.equalTo(50)
-            $0.centerX.equalToSuperview()
-        }
-        
         button.snp.makeConstraints {
-            $0.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(30)
             $0.left.equalTo(30)
             $0.right.equalTo(-30)
             $0.height.equalTo(50)
@@ -83,14 +56,18 @@ final class HotspotConfigurationViewController: UIViewController, UIViewControll
         wifiInfoLabel.snp.makeConstraints {
             $0.left.equalTo(20)
             $0.right.equalTo(-20)
-            $0.bottom.equalTo(self.view.safeAreaLayoutGuide)
             $0.top.equalTo(button.snp.bottom).offset(40)
+            $0.height.equalTo(50)
         }
     }
     
     func bindRx() {
         button.rx.tap
             .subscribe(onNext: viewModel.connectToHotspot)
+            .disposed(by: disposeBag)
+        
+        viewModel.inCompleted
+            .bind(to: wifiInfoLabel.rx.text)
             .disposed(by: disposeBag)
     }
 }
--- SwiftPractice/ViewModel/Signing & Capabilities/HotspotConfigurationViewModel.swift
+++ SwiftPractice/ViewModel/Signing & Capabilities/HotspotConfigurationViewModel.swift
@@ -14,44 +14,17 @@ final class HotspotConfigurationViewModel {
     
     let inCompleted = PublishRelay<String>()
     
-    func getWiFiInformation() {
-        NEHotspotHelper.register(options: nil, queue: DispatchQueue.main) { (command) in
-            if let wifiList = command.networkList {
-                for wifi in wifiList {
-                    let ssid = wifi.ssid
-                    let bssid = wifi.bssid
-                    let signalStrength = wifi.signalStrength
-
-                    print("SSID: \(ssid)")
-                    print("BSSID: \(bssid)")
-                    print("Signal Strength: \(signalStrength)")
-                }
-            }
-        }
-    }
-    
-    func connectToHotspot(ssid: String, password: String) {
-        let hotspotConfig = NEHotspotConfiguration(ssid: ssid, passphrase: password, isWEP: false)
-        hotspotConfig.joinOnce = true
-        
-        NEHotspotConfigurationManager.shared.apply(hotspotConfig) { error in
-            if let error = error {
-                print("Failed to connect to Wi-Fi hotspot: \(error.localizedDescription)")
-            } else {
-                print("Successfully connected to Wi-Fi hotspot")
-            }
-        }
-    }
-    
     func connectToHotspot() {
         let hotspotConfig = NEHotspotConfiguration(ssid: "욱", passphrase: "1234567890", isWEP: false)
         hotspotConfig.joinOnce = true
         
         NEHotspotConfigurationManager.shared.apply(hotspotConfig) { error in
             if let error = error {
-                print("Failed to connect to Wi-Fi hotspot: \(error.localizedDescription)")
+                print("핫스팟 연결 실패")
+                self.inCompleted.accept("Failed to connect to Wi-Fi hotspot: \(error.localizedDescription)")
             } else {
-                print("Successfully connected to Wi-Fi hotspot")
+                print("핫스팟 연결 성공")
+                self.inCompleted.accept("Successfully connected to Wi-Fi hotspot")
             }
         }
     }
